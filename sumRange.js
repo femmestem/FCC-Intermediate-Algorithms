108437fe65ff104ee46ce262dde2e44c442a09bb
Christine L. Feaster <feasterc@gmail.com>
2016-03-04 16:55:44 -0800

    [WIP] Get array of range for 'Sum Nums in Range'

--- sumRange.js
+++ sumRange.js
@@ -2,18 +2,24 @@
 // We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
 
 // The lowest number will not always come first.
-
 function sumAll(arr) {
-    var sortedArr;
-    var sum;
-
     // sort array
-    // add numbers between a to b to array
+    var sortedArr = arr.sort(function(a, b) {
+        return a - b;
+    });
+    var rangeArr = [];
+    var sum = 0;
+
+    // get numbers in range a..b
+    // add numbers in range a..b to array
+    for (var i=sortedArr[0], l = sortedArr[1]; i<=l; i++) {
+        rangeArr.push(i);
+    }
     // sum all elements of array
-    // return sum
-    
-  return sum;
+    console.log(rangeArr);
+  // return sum;
 }
 
 console.log(sumAll([4, 1]));
+console.log(sumAll([10, 5]));
 console.log(sumAll([4, 1]) == 10);
