37063013ec3834e263b252ae1d11e876d27921fb
Christine L. Feaster <feasterc@gmail.com>
2016-03-04 16:10:54 -0800

    Add pseudocode for challenge 'Sum Nums in Range'

---
+++ sumRange.js
@@ -0,0 +1,19 @@
+// Sum All Numbers in a Range
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+// The lowest number will not always come first.
+
+function sumAll(arr) {
+    var sortedArr;
+    var sum;
+
+    // sort array
+    // add numbers between a to b to array
+    // sum all elements of array
+    // return sum
+    
+  return sum;
+}
+
+console.log(sumAll([4, 1]));
+console.log(sumAll([4, 1]) == 10);
